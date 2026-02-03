d3b2ac71528d62d7158a672e94ec6596d18a762a
Anzymanz <anzymanz@users.noreply.github.com>
2026-02-03 20:09:39 +0000

    Honor all catalog filter params

--- capture.py
+++ capture.py
@@ -468,11 +468,10 @@ class CaptureWorker:
                                             q = urllib.parse.parse_qs(parsed.query)
                                             include_inactive = bool(self.cfg.get('include_inactive', False))
                                             requestable_only = bool(self.cfg.get('requestable_only', True))
+                                            in_stock_only = bool(self.cfg.get('in_stock_only', False))
                                             q['includeInactive'] = ['true' if include_inactive else 'false']
-                                            if requestable_only:
-                                                q['requestableOnly'] = ['true']
-                                            else:
-                                                q.pop('requestableOnly', None)
+                                            q['requestableOnly'] = ['true' if requestable_only else 'false']
+                                            q['requireAvailableStock'] = ['true' if in_stock_only else 'false']
                                             new_query = urllib.parse.urlencode(q, doseq=True)
                                             base_url = urllib.parse.urlunparse(parsed._replace(query=new_query))
                                             if base_url != (base_payload.get('url') or ''):
